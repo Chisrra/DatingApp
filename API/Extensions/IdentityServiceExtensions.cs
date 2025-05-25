50c9c0aea7f62d9efe737b0d3a4e3000940b7284
chisrra <chisrra-code@hotmail.com>
2025-05-24 18:17:44 -0600

    Parcial06 - Update start up class

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,12 +1,20 @@
 namespace API.Extensions;
 using System.Text;
+using API.Data;
+using API.DataEntities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddIdentityCore<AppUser>(opt => opt.Password.RequireNonAlphanumeric = false)
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
@@ -19,6 +27,7 @@ public static class IdentityServiceExtensions
                     ValidateAudience = false
                 };
             });
+
         return services;
     }
 }
